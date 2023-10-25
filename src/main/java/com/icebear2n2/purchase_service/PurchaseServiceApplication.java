51b4d570268775c1136f12a0667969bd7eb15afa
icebear2n2 <icebear2n2@gmail.com>
2023-10-25 15:46:44 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/icebear2n2/purchase_service/PurchaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.icebear2n2.purchase_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseServiceApplication.class, args);
+    }
+
+}
