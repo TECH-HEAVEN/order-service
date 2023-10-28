2f8ebc8dcca6ecf24d01e41e412e94528360b1f7
icebear2n2 <icebear2n2@gmail.com>
2023-10-28 11:23:13 +0900

    feat&fix: Cart, CartItem 리포지토리 생성 및 product 리포지토리 필드 전체 삭제

--- src/main/java/com/icebear2n2/purchaseService/domain/repository/CartRepository.java
+++ src/main/java/com/icebear2n2/purchaseService/domain/repository/CartRepository.java
@@ -3,5 +3,8 @@ package com.icebear2n2.purchaseService.domain.repository;
 import com.icebear2n2.purchaseService.domain.entity.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
 }
