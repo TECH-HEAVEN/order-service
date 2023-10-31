83ebe9d100c9ffa34aadd299ff7788864bed4dca
icebear2n2 <icebear2n2@gmail.com>
2023-10-31 11:38:33 +0900

    feat: AppConfig 설정

---
+++ src/main/java/com/icebear2n2/orderService/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.icebear2n2.orderService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Random random() {
+        return new Random();
+    }
+
+}
